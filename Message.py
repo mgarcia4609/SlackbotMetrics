9daf4c6793b3b7e74c869619aa9afcd5a17d310e
Garcia <Michael.Garcia@tylertech.com>
2020-04-09 22:43:13 -0500

    add Channel and Message Class

--- Message.py
+++ Message.py
@@ -1,1 +1,14 @@
 from slacker import Slacker
+
+class Message:
+    TimeStamp = 0.0
+    ParentTimeStamp = 0.0
+    UserId = ""
+    UserName = ""
+
+    def __init__(self, message):
+
+        self.Timestamp = message.Timestamp
+        self.ParentTimeStamp = message.ParentTimeStamp
+        self.UserId = message.UserId
+        self.UserName = message.UserName
